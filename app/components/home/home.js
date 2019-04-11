cead5dfe84a549e86f5a078a308153926f9d6195
klari <claricesaraivaandrade@gmail.com>
2019-04-11 16:20:24 -0300

    up

--- app/components/home/home.js
+++ app/components/home/home.js
@@ -8,52 +8,20 @@ angular.module('app')
 
         // controlador do noticiasAPI que serve ao
 
-        controller: function ($scope, $http, $location, requestServiceUrl, lancamentosAPI, categoriesAPI) {
+        controller: function ($scope, $http, $location, getLanceAPI, getNotiAPI) {
 
-            //let noticias = requestServiceUrl.getNoticias();
-            //console.log(noticias);
-            //$scope.clicke = function (noticia) {
-            //    alert(requestServiceUrl.getNoticias());
-            //}
+            getNotiAPI.getNoticias().then((noti) => {
+                $scope.notici = noti;
+                console.log("1",noti);
+                $scope.$apply();
+            });
 
+            getLanceAPI.getLancamentos().then((lance) => {
+                $scope.lancam = lance;
+                console.log("1",lance);
+                $scope.$apply();
 
-            let finan;
-            (function () {
-                requestServiceUrl.getNoti().then(function (response) {
-                    $scope.notici = response.data.noticias;
-                }).catch(function (erro) {
-                    console.log("socorro: ", erro.statusText)
-                })
-            }());
-
-            (function () {
-                lancamentosAPI.getLance().then(function (response) {
-                    $scope.lancam = response.data.lancamentos;
-                    finan = response.data.lancamentos;
-                }).catch(function (erro) {
-                    console.log("socorro: ", erro.statusText)
-                })
-            }());
-
-            $scope.getTotal = function(){
-                var total = 0;
-                for(var i = 0; i < $scope.lancam.length; i++){
-                    var v = $scope.lancam[i];
-                    total += Math.max(Math.round(v.valor));
-                }
-                return total;
-            };
-
-            // eu quero a validacao aqui
-            $scope.entrar = function (info) {
-                if (($scope.pass === "test") && ($scope.usu === "test")){
-                    console.log("simm");
-                    $location.path("/user/test")
-                } else {
-                    console.log("no");
-                    $location.path("/invalid")
-                }
-            };
+            });
 
             $scope.soma = function () {
                 var totalRe = 0;
